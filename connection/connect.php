617bbec16f78e4bd99d2154b4bfb25b45511905f
jaykhatri0710 <jay.khatri@somaiya.edu>
2021-09-05 21:26:28 +0530

    Connetion changes

--- connection/connect.php
+++ connection/connect.php
@@ -1,10 +1,10 @@
 <?php
 
 //main connection file for both admin & front end
-$servername = "localhost"; //server
-$username = "root"; //username
-$password = ""; //password
-$dbname = "online_rest";  //database
+$servername = "remotemysql.com"; //server
+$username = getenv('USERNAME'); //username
+$password = getenv('PASSWORD'); //password
+$dbname = getenv('USERNAME');  //database
 
 // Create connection
 $db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
